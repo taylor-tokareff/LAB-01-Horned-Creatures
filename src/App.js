2b49d8ba66a20ac0117867b4074133c08ff669f6
taylor-tokareff <taylortokareff@gmail.com>
2021-04-27 14:58:25 -0700

    wire in creature search component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './Header.js';
 import Footer from './Footer.js';
 import CreatureList from './CreatureList.js';
 import images from './Creatures.js';
+import CreatureSearch from './CreatureSearch.js';
 
 class App extends Component {
 
@@ -15,6 +16,7 @@ class App extends Component {
         <Header />
 
         <main>
+          <CreatureSearch />
           <CreatureList creatures={images} />
         </main>
 
---
+++ src/CreatureSearch.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class CreatureSearch extends Component {
+  render() {
+    return (
+
+      <form> <button> </button> </form>
+
+    );
+  }
+}
